5178dea41058947920a8444a45d421b8de29a254
yimobing <1614644937@qq.com>
2021-07-10 17:24:29 +0800

    新增：utilities.getDataset(dom) 获取dataset的值(兼容ie11-)

--- assets/neatui/js/neatui-functions.js
+++ assets/neatui/js/neatui-functions.js
@@ -533,6 +533,35 @@ var utilities = {
     },
 
 
+    /**
+     * 原生JS获取dataset的值(兼容ie11-)
+     * 示例：要取某个data-*(比如data-bh)的值 <=> getDataset(DOM元素).bh
+     * @param {HTMLDOM} ele DOM元素
+     * @returns 返回dataset集合.
+     */
+    getDataset: function(ele){
+        if(ele.dataset){
+            return ele.dataset;
+        }else{
+            var attrs = ele.attributes,//元素的属性集合
+                dataset = {},
+                name,
+                matchStr;
+            for(var i = 0;i<attrs.length;i++){
+                //是否是data- 开头
+                matchStr = attrs[i].name.match(/^data-(.+)/);
+                if(matchStr){
+                    //data-auto-play 转成驼峰写法 autoPlay
+                    name = matchStr[1].replace(/-([\da-z])/gi,function(all,letter){
+                        return letter.toUpperCase();
+                    });
+                    dataset[name] = attrs[i].value;
+                }
+            }
+            return dataset;
+        }
+    },
+
 
     /**
      * 获取当前网络类型
