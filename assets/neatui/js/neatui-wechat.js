c7fcf31677cbec05c9a6c90cb41c6435cac6d2ac
yimobing <1614644937@qq.com>
2021-10-13 14:53:20 +0800

    修改exportWeb2Image()以兼容ie

--- assets/neatui/js/neatui-wechat.js
+++ assets/neatui/js/neatui-wechat.js
@@ -6,6 +6,21 @@
 
 (function($){
 
+	// Element.scrollTo() 兼容ie
+	if (!window.scrollTo) {
+		window.scrollTo = function(option) {
+			window.scrollLeft = option.left;
+			window.scrollTop = option.top;
+		};
+	}
+	if (!document.body.scrollTo) {
+		Element.prototype.scrollTo = function(option) {
+			this.scrollLeft = option.left;
+			this.scrollTop = option.top;
+		};
+	}
+	
+	//
 	var win = window;
 	var doc = document;
 	var $win = $(win);
