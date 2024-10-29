73f3564b73dd8cb67697f3f842e3d25b7338a344
yimobing <1614644937@qq.com>
2024-10-29 11:54:07 +0800

    更新 ajax控件注释及文档说明

--- assets/neatui/js/neatui-ajax.js
+++ assets/neatui/js/neatui-ajax.js
@@ -43,27 +43,33 @@
                 return;
             }
             var defaults = {
-                heading: "", //接口描述(中文)
-                debug: false, //是否启用调试模式,默认false. 调试模式下会把具体错误信息提示给用户看,非调试模式下只会给用户友好提示信息
-                async: false,
+                // 自定义的参数
+                heading: "", // 接口描述(中文)
+                debug: false, // 是否启用调试模式,默认false. 调试模式下会把具体错误信息提示给用户看,非调试模式下只会给用户友好提示信息
                 // add 20241017-1
                 clean: false, // 是否去掉请求地址中的某些后辍参数,默认false(可选)。注：目前正则还没写好,只能去掉链接地址中只有一个参数，多个参数无法只去掉某一个。
                 matches: [], // 请求地址中的后辍参数组成的数组,默认空数组(可选). 仅当 clean=true时有效.eg. matches['param'], 则url为'aabb.com?param=1&add=2' 将变成 'aabb.com?add=2'
 
-                type: "GET",
-                dataType: "html",
-                cache: false,
-                timeout: 10000, //请求超时时间(单位:毫秒计), 默认10秒
-                url: "",
-                data: { },
-                success: function(res){ },
-                error: function(res){
+                // JQuery Ajax 原生的参数说明
+                async: false, // 请求是否异步(可选)。原始的默认是true
+                type: "GET", // 请求类型(可选)。值：get 或 post。注：有 data 参数时，若使用 Mock.js 模拟数据则要设置 type='POST'
+                dataType: "html", // 服务器响应的数据类型(可选)
+                cache: false, // 浏览器是否缓存被请求页面(可选)。原始的默认是true
+                timeout: 10000, // 请求超时时间, 默认10秒(可选)。单位毫秒
+                url: "", // 接口地址字符串(必须)
+                data: { }, // 接口参数，即要发送到服务器的数据(可选)。注：有 data 参数时，若使用 Mock.js 模拟数据则要设置 type='POST'
+                success: function(res){ }, // 请求成功时运行的函数(可选)。 格式：success(result, status, xhr)
+                error: function(errs){ // 请求失败时运行的函数(可选)。格式：error(xhr, status, errs)
                     var debug = typeof this.debug == 'undefined' ? false : (this.debug.toString().toLocaleLowerCase() == 'true' ? true : false);
                     var action = utilities.getStringParams('action', this.url);
-                    utilities.toast(action, this.heading, res, "errors", debug ? false : true);
+                    utilities.toast(action, this.heading, errs, "errors", debug ? false : true);
                 },
-                beforeSend: function(XMLHttpRequest){ },
-                complete: function(XMLHttpRequest, textStatus){ }
+                beforeSend: function (xhr) {  // 发送请求前运行的函数(可选)
+                    
+                },
+                complete: function (xhr, status) { // 请求完成时运行的函数(可选)。无论请求成功或失败都会执行本函数
+                    
+                }
             }
             var settings = $.extend(true, {}, defaults, options || {});
             // add 20241017-1
