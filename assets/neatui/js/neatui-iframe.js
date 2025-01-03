76ae8b2c5b7b5a013db38dba7fe8a54aef62e577
yimobing <1614644937@qq.com>
2025-01-03 21:05:00 +0800

    iframe自动嵌入页面新增返回及关闭按钮及自定义页面打开方式

--- assets/neatui/js/neatui-iframe.js
+++ assets/neatui/js/neatui-iframe.js
@@ -4,7 +4,7 @@
  * 调用控件即可直接嵌入一个iframe页面，浏览器窗口调整时，iframe子页面大小也会自动调整
  * Author：Mufeng
  * Date: 2024.10.08
- * Update: 2024.10.16
+ * Update: 2025.01.03
  */
 ; (function (root, factory) {
     if (typeof define === 'function' && define.amd) { // AMD规范
@@ -26,7 +26,7 @@
      * @param {HTML DOM Node} elem 绑定的节点。eg. '#ID节点', '.类名节点'
      * @param {Ojbect} options 参数配置项
      */
-     function NFrame(elem, options) {
+    function NFrame(elem, options) {
         // “构造函数模式”定义实例属性，这些属性是独立的，即改变某个实例的属性并不影响其它实例的属性
         // this.a = '';
         // this.b = '';
@@ -48,18 +48,21 @@
     function FrameInitialize(me, elem, options) {
         // console.log('初始化');
         var defaults = {
-            src: '', // 框架链接地址(必须)
+
+            src: "", // 框架链接地址(必须)
             refresh: false, // 是否刷新绑定的节点中的页面，默认false(可选)。值为true 时将重建页面，false 时不会重建页面。注：当点击不同按钮时要在同一个绑定的节点下面显示不同的页面，请将本参数设为true。
+
             // 指定框架宽高。注：当框架实际宽高<=0时，系统将自动把浏览器视窗高度作为框架宽高。
-            width: 'auto', // 框架宽度，默认auto表示系统自动计算(可选)。接受百分比如50%、带或不带单位的数值型如100px。
-            height: 'auto', // 框架高度，默认auto表示系统自动计算(可选)。接受百分比如50%、带或不带单位的数值型如100px。
-            // 自定义框架左侧、右侧距离。仅当 width != 'auto' 时有效
-            left: 'auto', // 框架以外左侧距离，默认auto(可选)。接受带或不带单位的数值型如100px。值：auto 或 0 系统将自动计算。
-            right: 'auto', // 框架以外右侧距离，默认auto(可选)。接受带或不带单位的数值型如100px。值：auto 或 0 系统将自动计算。
-            // 自定义框架顶部、底部距离。仅当 height != 'auto' 时有效
-            top: 'auto', // 框架以外顶部距离，默认auto(可选)。接受带或不带单位的数值型如100px。值：auto 或 0 系统将自动计算。
-            bottom: 'auto', // 框架以外底部距离，默认auto(可选)。接受带或不带单位的数值型如100px。值：auto 或 0 系统将自动计算。
-            // 框架误差纠正，若不想启用误差纠正功能，请设置参数 width, height 为具体数值即可。注：以下参数当 width = 'auto' 时宽度纠正参数失效，height = 'auto' 时高度纠正参数失效。
+            width: "auto", // 框架宽度，默认auto表示系统自动计算(可选)。接受百分比如50%、带或不带单位的数值型如100px。
+            height: "auto", // 框架高度，默认auto表示系统自动计算(可选)。接受百分比如50%、带或不带单位的数值型如100px。
+            // 自定义框架左侧、右侧距离。仅当 width != "auto" 时有效
+            left: "auto", // 框架以外左侧距离，默认auto(可选)。接受带或不带单位的数值型如100px。值：auto 或 0 系统将自动计算。
+            right: "auto", // 框架以外右侧距离，默认auto(可选)。接受带或不带单位的数值型如100px。值：auto 或 0 系统将自动计算。
+            // 自定义框架顶部、底部距离。仅当 height != "auto" 时有效
+            top: "auto", // 框架以外顶部距离，默认auto(可选)。接受带或不带单位的数值型如100px。值：auto 或 0 系统将自动计算。
+            bottom: "auto", // 框架以外底部距离，默认auto(可选)。接受带或不带单位的数值型如100px。值：auto 或 0 系统将自动计算。
+
+            // 框架误差纠正，若不想启用误差纠正功能，请设置参数 width, height 为具体数值即可。注：以下参数当 width = "auto" 时宽度纠正参数失效，height = "auto" 时高度纠正参数失效。
             corrected: true, // 是否启用框架宽高值误差纠正功能，默认true(可选)
             inaccuracy: 0, // 统一设置框架宽高误差值，默认0(可选)。建议值14，仅当 corrected = true 且 deviation.enable = false 时有效。注：1.系统将在框架宽高基础上再减去该参数的值。2.参数值为0时，自动将把浏览器滚动条宽度作为误差值。
             deviation: { // 分开设置框架宽高误差值(可选)。仅当 corrected 且 deviation.enable = true 时 有效。生效后参数 inaccuracy 失效。
@@ -67,24 +70,111 @@
                 horizontal: 0, // 水平方向宽度误差值，默认0(可选)。注：系统将在框架宽度基础上再减去该参数的值。
                 vertical: 0 // 垂直方向高度误差值，默认0(可选)。注：系统将在框架高度基础上再减去该参数的值。
             },
-            // 回调函数
+
+            // 视图配置
+            visible: true, // 是否默认显示绑定节点内容，默认true(可选)。如果希望初始化控件时不显示绑定节点，可将值置为false，而后手动显示节点内容。一般情况下，请勿置为false。
+            fatherSelector: "", // 绑定节点的父节点，默认空(可选)。eg. "#id", ".classname"。有时在绑定节点上方或下方可能会有自定义的内容，布局时可能要在绑定节点外层添加一个父节点，便可将父节点填在此处。父节点和绑定节点显示与隐藏是联动一致的，要么同时显示要么同时隐藏。
+            caption: "", // 标题，默认空(可选)
+
+            // 框架打开方式(可选)
+            openWay: {
+                method: "embed", // 打开方式，默认embed(可选)。值： embed 嵌入, newtab 新选项卡窗口, control 按住ctrl键的同时以新选项卡窗口打开页面(仅当 pressKeyCtrl 为true时有效)(此时页面嵌入到绑定节点且在新窗口中打开页面)
+                pressKeyCtrl: false // 是否按住ctrl键，默认false(可选)。说明：值取自界面按钮点击事件属性 event.ctrlKey，如要确认是否按住ctrl键，请把 event.ctrlKey 传入即可
+            },
+
+            // 返回按钮(可选)
+            goback: {
+                enable: false, // 是否显示返回按钮，默认false(可选)
+                text: "返回", // 返回按钮的文字，默认“返回”(可选)
+                position: "absolute", // 定位方式，默认绝对定位(可选)
+                offset: { // 偏移量(可选)
+                    top: 0,
+                    left: 0
+                },
+                callback: null // 点击返回按钮的回调，默认null(可选)
+            },
+
+            // 关闭按钮(可选)
+            close: {
+                enable: false, // 是否显示关闭按钮，默认false(可选)
+                text: "关闭", // 关闭按钮的文字，默认“关闭”(可选)
+                position: "absolute", // 定位方式，默认绝对定位(可选)
+                offset: { // 偏移量(可选)
+                    top: 0,
+                    right: 0
+                },
+                callback: null // 点击关闭按钮的回调，默认null(可选)
+            },
+
+            // 其它配置
             resize: null // 浏览器窗口大小变化时的回调函数，默认null(可选)
         }
+
         var settings = helpers.extend(true, {}, defaults, options || {}); // 合并对象
         me.$defaults = defaults;
         me.$opts = settings; // 方便其它地方直接调用
-        // 创建内容
+
+        // 取参数
+        var linkUrl = me.$opts.src,
+            open_method = me.$opts.openWay.method;
+        
+        // 框架打开方式
+        if (open_method == 'newtab') {
+            window.open(linkUrl, '_blank');
+            return;
+        }
+        else if (open_method == 'control') {
+            if (me.$opts.openWay.pressKeyCtrl) {
+                window.open(linkUrl, '_blank');
+            }
+        }
+
+        // 定义样式名
         var randChar = helpers.getRandomChar();
         var rootClassName = 'fm__root_' + randChar,
             skeletonClassName = 'fm__body_' + randChar;
+        // 返回按钮样式
+        var gobackPosition = me.$opts.goback.position,
+            gobackStyle = 'position: ' + gobackPosition + ';',
+            gobackOffset = me.$opts.goback.offset;
+        if (gobackPosition != 'relative') {
+            for (var v in gobackOffset) {
+                if (gobackOffset.hasOwnProperty(v)) {
+                    var value = gobackOffset[v].toString().replace(/(px|rem|vw|vh)/g, '');
+                    if (value.indexOf('%') < 0) value += 'px';
+                    gobackStyle += ' ' + v + ': ' + value + ';';
+                }
+            }
+        }
+        gobackStyle = ' style="' + gobackStyle + '"';
+        // 关闭按钮样式
+        var closePosition = me.$opts.close.position,
+            closeStyle = 'position: ' + closePosition + ';',
+            closeOffset = me.$opts.close.offset;
+        if (closePosition != 'relative') {
+            for (var v in closeOffset) {
+                if (closeOffset.hasOwnProperty(v)) {
+                    var value = closeOffset[v].toString().replace(/(px|rem|vw|vh)/g, '');
+                    if (value.indexOf('%') < 0) value += 'px';
+                    closeStyle += ' ' + v + ': ' + value + ';';
+                }
+            }
+        }
+        closeStyle = ' style="' + closeStyle + '"';
+        // 创建内容
         var allHtml = [
             '<div class="ne__frame ' + rootClassName + '">',
-                '<iframe class="ne__fm_body ' + skeletonClassName + '" id="ne-fm-body" name="ne-fm-name" scrolling="yes" frameborder="0" src="' + me.$opts.src + '"></iframe>',
+                me.$opts.goback.enable == false ? '' : '<div class="ne__fm_goback" title="' + me.$opts.goback.text + '"' + gobackStyle + '>' + me.$opts.goback.text + '</div>',
+                me.$opts.caption.toString().replace(/\s+/g, '') === '' ? '' : '<div class="ne__fm_caption">' + me.$opts.caption + '</div>',
+                me.$opts.close.enable == false ? '' : '<div class="ne__fm_close" title="' + me.$opts.close.text + '"' + closeStyle + '>' + me.$opts.close.text + '</div>',
+                '<iframe class="ne__fm_body ' + skeletonClassName + '" id="ne-fm-body" name="ne-fm-name" scrolling="yes" frameborder="0" src="' + linkUrl + '"></iframe>',
             '</div>'
         ].join('\r\n');
+        
         var classId = elem.toString().replace(/(#|\.)/g, '');
         // console.log('aaa：', document.getElementsByClassName(classId).length);
         // console.log('bbb：', document.getElementById(classId));
+
         // 判断节点是否存在
         var userNode = document.getElementById(classId) != null ?
             document.getElementById(classId) :
@@ -109,10 +199,37 @@
         }
         else {
             userNode.innerHTML = ''; // 清空内容
+            // 显示绑定节点及父节点
+            if (me.$opts.visible) {
+                // 显示绑定节点
+                var elStyles = userNode.getAttribute('style');
+                if (elStyles != null && elStyles.toString().replace(/\s+/g, '').indexOf('display:none') >= 0) {
+                    userNode.style.display = '';
+                }
+                // 显示父节点
+                var elFather = helpers.getNodeByClassId(me.$opts.fatherSelector);
+                if (elFather != null) {
+                    var fatherStyles = elFather.getAttribute('style');
+                    if (fatherStyles  != null && fatherStyles.toString().replace(/\s+/g, '').indexOf('display:none') >= 0) {
+                        elFather.style.display = '';
+                    }
+                }
+            }
             // 重建内容
             helpers.appendHTML(allHtml, userNode);
             methods._setControlStyle(me, userNode, rootClassName, skeletonClassName);
         }
+
+        // 返回按钮点击及回调事件
+        if (me.$opts.goback.enable) {
+            methods._closeFrame(me, userNode, rootClassName, 'goback');
+        }
+
+        // 关闭按钮点击及回调事件
+        if (me.$opts.close.enable) {
+            methods._closeFrame(me, userNode, rootClassName, 'close');
+        }
+
         // 回调
         if (me.$opts.resize) {
             window.addEventListener('resize', methods._handleWindowChange(me));
@@ -221,6 +338,43 @@
 
 
         /**
+         * 关闭框架并执行回调
+         *  @param {Object} me 控件自身对象
+         * @param {HTMLDOM} 用户绑定的节点
+         * @param {String} root_class_name 根节点样式名
+         * @paam {String} ps_type 按钮类型。值： goback 返回按钮, close 关闭按钮
+         */
+         _closeFrame: function (me, user_node, root_class_name, ps_type) {
+            var rootNode = document.getElementsByClassName(root_class_name)[0];
+            var btnClassName = '';
+            if (ps_type == 'goback') btnClassName = 'ne__fm_goback';
+            else if (ps_type == 'close') btnClassName = 'ne__fm_close';
+            var clickNode = rootNode.getElementsByClassName(btnClassName);
+            if (clickNode.length == 0) return;
+            clickNode[0].addEventListener('click', function (e) {
+                user_node.innerHTML = ''; // 清空内容
+                user_node.setAttribute('style', 'display: none'); // 隐藏绑定节点
+                // 隐藏父节点
+                var elFather = helpers.getNodeByClassId(me.$opts.fatherSelector);
+                if (elFather != null) { // 父节点存在时
+                    var fatherStyles = elFather.getAttribute('style');
+                    if (fatherStyles != null && fatherStyles.toString().replace(/\s+/g, '').indexOf('display:none') < 0) {
+                        elFather.style.display = 'none';
+                        // elFather.setAttribute('style', 'display: none');
+                    }
+                }
+                if (ps_type == 'goback' && me.$opts.goback.callback) {
+                    me.$opts.goback.callback();
+                }
+                if (ps_type == 'close' && me.$opts.close.callback) {
+                    me.$opts.close.callback();
+                }
+            }); 
+        },
+
+         
+         
+        /**
          * 浏览器窗口大小发生变化时
          * @param {Object} me 控件自身对象
          */
@@ -266,6 +420,22 @@
             return str;
         },
 
+
+        /**
+         * 根据样式名或ID名获取某个节点
+         * @param {String} ps_selector 样式名或ID名选择器。eg. '#id', '.classname'
+         * @param {HtmlElement || null} 返回样式名或ID名选择器对应的元素。如不存在，则返回null
+         */
+        getNodeByClassId: function (ps_selector) {
+            var classId = ps_selector.toString().replace(/(#|\.)/g, '');
+            if (classId == null || classId.toString().replace(/\s+/g, '') === '') return null;
+            var node = document.getElementById(classId) != null ?
+                document.getElementById(classId) :
+                (
+                    document.getElementsByClassName(classId).length == 0 ? null : document.getElementsByClassName(classId)[0]
+                );
+            return node;
+        },
 
 
         /**
